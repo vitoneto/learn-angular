98963a30add7d78758560f1736cb3f002ee65b97
vitoneto <vitomanoelneto1997@gmail.com>
2025-01-16 19:01:13 -0300

    feat: Control Flow in Components - @for.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,11 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    @if(isLoggedIn) {
-      <p>Welcome back, friend!</p>
-    }
-    @if(isServerRunning) {
-      <p>Yes, the server is running</p>
-    } @else {
-      <p>No, the server is not running</p>
+    @for(user of users; track user.id) {
+      {{ user.name }}
     }
   `,
 })
 export class AppComponent {
-  isLoggedIn: boolean = true;
-  isServerRunning: boolean = false;
+  users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}];
 }
