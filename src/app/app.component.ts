99f5842211ffda8f0899855acfbb70b3aed06095
vitoneto <vitomanoelneto1997@gmail.com>
2025-01-21 17:44:23 -0300

    feat: Property Binding in Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    @for(user of users; track user.id) {
-      {{ user.name }}
-    }
+    <div [contentEditable]="isEditable"></div>
   `,
 })
 export class AppComponent {
-  users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}];
+  isEditable: boolean = true;
 }
